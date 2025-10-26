77d6921e37c691aeb8bf91095f79c816b1e27eb8
MarcoAPelonero <marco.pelonero.work@gmail.com>
2025-10-26 15:36:46 +0100

    Adding the template for ex4

---
+++ EX4/src/exPoints.cpp
@@ -0,0 +1,58 @@
+#include "exPoints.hpp"
+
+#include <chrono>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+#include <array>
+#include <numeric>
+#include <cmath>
+#include <thread>
+#include <atomic>
+#include <algorithm>
+#include <sstream>
+#include <cstdlib>
+
+void exPointOne() {
+    std::cout << "Executing Exercise Point One" << std::endl;
+}
+
+void exPointTwo() {
+    std::cout << "Executing Exercise Point Two" << std::endl;
+}
+
+void exPointThree() {
+    std::cout << "Executing Exercise Point Three" << std::endl;
+}
+
+
+void exPointFour() {
+    std::cout << "Executing Exercise Point Four" << std::endl;
+}
+
+void exPointFive() {
+    std::cout << "Executing Exercise Point Five" << std::endl;
+}
+
+
+void exPointSix() {
+    std::cout << "Executing Exercise Point Six" << std::endl;
+}
+
+void exPointSeven() {
+    std::cout << "Executing Exercise Point Seven" << std::endl;
+}
+
+void exPointEight() {
+    std::cout << "Executing Exercise Point Eight" << std::endl;
+}
+
+void exPointNine() {
+    std::cout << "Executing Exercise Point Nine" << std::endl;
+}
+
